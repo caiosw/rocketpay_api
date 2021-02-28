42010dfff2ca1b14c8d6b22009ef5213196141f3
caiosw <caiosw@gmail.com>
2021-02-28 17:14:39 -0300

    insert transaction to table

--- lib/rocketpay/accounts/transaction.ex
+++ lib/rocketpay/accounts/transaction.ex
@@ -1,20 +1,29 @@
 defmodule Rocketpay.Accounts.Transaction do
   alias Ecto.Multi
-  alias Rocketpay.Repo
+  alias Rocketpay.{Repo, Transaction}
   alias Rocketpay.Accounts.Operation
   alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
 
   def call(%{"from" => from_id, "to" => to_id, "value" => value}) do
     withdraw_params = build_params(from_id, value)
     deposit_params = build_params(to_id, value)
+    transaction = %{account_id_from: from_id, account_id_to: to_id, value: value}
 
     Multi.new()
     |> Multi.merge(fn _changes -> Operation.call(withdraw_params, :withdraw) end)
     |> Multi.merge(fn _changes -> Operation.call(deposit_params, :deposit) end)
+    |> Multi.merge(fn _changes -> insert_transaction(transaction) end)
     |> run_transaction()
   end
 
   defp build_params(id, value), do: %{"id" => id, "value" => value}
+
+  defp insert_transaction(transaction) do
+    transaction = Transaction.changeset(transaction)
+
+    Multi.new()
+    |> Multi.run(:insert_transaction, fn repo, _changes -> repo.insert(transaction) end)
+  end
 
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
